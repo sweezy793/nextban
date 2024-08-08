e5cbda576643ec01383df14087219407c98b9522
sweezy793 <sarthaks793@gmail.com>
2024-08-09 02:05:23 +0530

    Basic login form setup done

---
+++ src/components/auth/FormSuccess.tsx
@@ -0,0 +1,16 @@
+import { BsCheck2Circle } from "react-icons/bs";
+
+interface FormSuccessProps {
+  message?: string;
+}
+
+export const FormSuccess = ({ message }: FormSuccessProps) => {
+  if (!message) return null;
+
+  return (
+    <div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500">
+      <BsCheck2Circle className="h-4 w-4" />
+      {message}
+    </div>
+  );
+};
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,6 +1,37 @@
+"use client";
+
+import { useForm } from "react-hook-form";
 import { CardWrapper } from "./CardWrapper";
+import * as z from "zod";
+import { LoginSchema } from "../../../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/Button";
+import { FormError } from "./FormError";
+import { FormSuccess } from "./FormSuccess";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    // basically converts the zod schema to a typescript type
+    resolver: zodResolver(LoginSchema), // zodResolver is the bridge between useForm hook and zod. allows react-hook-form to validate data using zod schema
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div>
       <CardWrapper
@@ -9,7 +40,52 @@ export const LoginForm = () => {
         backButtonHref="/sign-up"
         showSocial
       >
-        Login Form
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="john.doe@example.com"
+                        type="email"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="*********"
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormError />
+            <FormSuccess />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
       </CardWrapper>
     </div>
   );
